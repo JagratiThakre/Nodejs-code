e21829ba70052f6d42b37dfbf5edb6d615e44e0e
Jagrati Thakre <148617354+JagratiThakre@users.noreply.github.com>
2024-02-22 16:40:22 +0530

    Create var2.js

---
+++ var2.js
@@ -0,0 +1,7 @@
+console.log("Program to understand variables in jS")
+var a=54 //a contain 54
+console.log(a)
+a="Tanu"
+console.log(a)
+//let 8tanu=9 //not allowed because let,var,const cannot start with a number
+//let var=7// not allowed because var is a reserved word in JS
